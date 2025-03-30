72c1f120fbfb890d6c3d7c1b6dd741a95dcb1276
mathdugre <16450132+mathdugre@users.noreply.github.com>
2025-03-30 01:12:18 -0400

    exp

--- Docker/ants-vprec-exclude.Dockerfile
+++ Docker/ants-vprec-exclude.Dockerfile
@@ -53,12 +53,12 @@ RUN : \
 # Build ITK
 RUN : \
     && cd /tmp/itk/build \
-    && make -j \
+    && make -j1 \
     && :
 # Install ITK
 RUN : \
     && cd /tmp/itk/build \
-    && make install \
+    && make -j1 install \
     && :
 
 # ANTs paper-base with ITK verificarlo compilation.
@@ -92,12 +92,12 @@ RUN : \
 # Build ANTs
 RUN : \
     && cd /tmp/ants/build \
-    && make -j \
+    && make -j1 \
     && :
 # Install ANTs
 RUN : \
     && cd /tmp/ants/build/ANTS-build \
-    && make install \
+    && make -j1 install \
     && :
 
 # Need to set library path to run tests
--- sbatch/run-all.sh
+++ sbatch/run-all.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 set -u
 
-export PROJECT_DIR=/mnt/lustre/mathdugre/fast-interpolate
-SIF_DIR=/mnt/lustre/mathdugre/containers
-export TEMPLATEFLOW_DIR=$HOME/.cache/templateflow
+module load apptainer
+
+export PROJECT_DIR=/home/mdugre/projects/rrg-glatard/mdugre/mri-registration-rp
+export SIF_DIR=/home/mdugre/projects/rrg-glatard/mdugre/containers
+export TEMPLATEFLOW_DIR=/home/mdugre/projects/rrg-glatard/mdugre/templateflow
+SLURM_OPTS="--account=rrg-glatard"
 
 # Datatset preparation
 cat << EOF
@@ -26,33 +29,29 @@ NUM_SUBJECTS=$(wc -l < subject_ids.txt)
 # ANTs Brain Extraction #
 #########################
 export SIF_IMG=${SIF_DIR}/ants-paper-base.simg
-job1=$(sbatch --parsable --array=1-${NUM_SUBJECTS} ./sbatch/antsBrainExtraction.sbatch)
+job1=$(sbatch --parsable --array=1-${NUM_SUBJECTS} ${SLURM_OPTS} ./sbatch/antsBrainExtraction.sbatch)
 
 #####################
 # ANTs Registration #
 #####################
-export APPTAINERENV_ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1  # Default value for pipelines
 export FIXED_IMG=/templateflow/tpl-MNI152NLin2009cAsym/tpl-MNI152NLin2009cAsym_res-01_desc-brain_T1w.nii.gz
-
-export SIF_IMG=${SIF_DIR}/ants-vprec-no_metric.simg
+export SIF_IMG=${SIF_DIR}/ants-vprec-no_metrics.simg
 
 # Binary64
 export EXPERIMENT_NAME="binary64"
 export APPTAINERENV_VFC_BACKENDS="libinterflop_ieee.so"
-export THRESHOLD="1e-6"
-sbatch --dependency=afterok:$job1 --array=1-${NUM_SUBJECTS} ./sbatch/antsRegistration.sbatch
+sbatch --dependency=afterok:$job1 --array=1-${NUM_SUBJECTS} ${SLURM_OPTS} ./sbatch/antsRegistration-0111.sbatch
 
 # Space search
-for precision in {23..1}; do
+for precision in {23..7}; do
     for range in {8..7}; do
-        # EXP=$(python3 -c "import math; print(math.floor(math.log10(2)*$precision) - 1)")
-        # export THRESHOLD="1e-${EXP}"
-        export THRESHOLD="1e-6"
-
         export EXPERIMENT_NAME="r${range}-p${precision}"
         export APPTAINERENV_VFC_BACKENDS="libinterflop_vprec.so --range-binary32=$range --precision-binary32=$precision --range-binary64=$range --precision-binary64=$precision"
-        # sbatch --job-name=${EXPERIMENT_NAME} --dependency=afterok:$job1 --array=1-${NUM_SUBJECTS} ./sbatch/antsRegistration.sbatch
-        sbatch --job-name=${EXPERIMENT_NAME} --dependency=afterok:$job1 --array=1-${NUM_SUBJECTS} ./sbatch/antsRegistration-001.sbatch
-        sbatch --job-name=${EXPERIMENT_NAME} --dependency=afterok:$job1 --array=1-${NUM_SUBJECTS} ./sbatch/antsRegistration-011.sbatch
+        sbatch --job-name=${EXPERIMENT_NAME} --dependency=afterok:$job1 --array=1-${NUM_SUBJECTS} ${SLURM_OPTS} ./sbatch/antsRegistration-0001.sbatch
+        # sbatch --job-name=${EXPERIMENT_NAME} --dependency=afterok:$job1 --array=1-${NUM_SUBJECTS} ${SLURM_OPTS} ./sbatch/antsRegistration-0011.sbatch
+        # sbatch --job-name=${EXPERIMENT_NAME} --dependency=afterok:$job1 --array=1-${NUM_SUBJECTS} ${SLURM_OPTS} ./sbatch/antsRegistration-0100.sbatch
+        # sbatch --job-name=${EXPERIMENT_NAME} --dependency=afterok:$job1 --array=1-${NUM_SUBJECTS} ${SLURM_OPTS} ./sbatch/antsRegistration-0110.sbatch
+        # sbatch --job-name=${EXPERIMENT_NAME} --dependency=afterok:$job1 --array=1-${NUM_SUBJECTS} ${SLURM_OPTS} ./sbatch/antsRegistration-0111.sbatch
+        # sbatch --job-name=${EXPERIMENT_NAME} --dependency=afterok:$job1 --array=1-${NUM_SUBJECTS} ${SLURM_OPTS} ./sbatch/antsRegistration-1111.sbatch
     done
 done
